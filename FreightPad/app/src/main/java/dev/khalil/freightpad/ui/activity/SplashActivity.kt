39b8945187b6df12bb93d5f550b9b475ea393b3c
Khalil <khalilayache@gmail.com>
2020-04-14 19:01:24 -0300

    create Splash Activity and added Google Maps Api

--- FreightPad/app/src/main/java/dev/khalil/freightpad/ui/activity/SplashActivity.kt
+++ FreightPad/app/src/main/java/dev/khalil/freightpad/ui/activity/SplashActivity.kt
@@ -1,11 +1,13 @@
-package dev.khalil.freightpad
+package dev.khalil.freightpad.ui.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        startActivity(MapsActivity.createIntent(this))
+        finish()
     }
 }
